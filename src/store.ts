6334cc10e242ec34d622c9c7011c0c40fdbe2872
k1ego <ladonin05@mail.ru>
2024-06-21 18:28:33 +0300

    Перевел компонент на redux

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector, useStore } from 'react-redux';
 
+export type UserId = string;
+export type User = {
+	id: UserId;
+	name: string;
+	description: string;
+};
+
+const users: User[] = Array.from({ length: 3000 }, (_, index) => ({
+	id: `user${index + 11}`,
+	name: `User ${index + 11}`,
+	description: `Description for User ${index + 11}`,
+}));
+
+type UsersState = {
+	entities: Record<UserId, User>;
+	// дефолтная сортировка, которая приходит с сервера
+	ids: UserId[];
+	// для выбора юзера и хранения его в redux состоянии
+	selectedUserId: UserId | undefined;
+};
+
 type CounterState = {
 	counter: number;
 };
@@ -9,6 +30,25 @@ export type CounterId = string;
 
 type State = {
 	counters: Record<CounterId, CounterState | undefined>;
+	users: UsersState;
+};
+
+export type UserSelectedAction = {
+	type: 'userSelected';
+	payload: {
+		userId: UserId;
+	};
+};
+
+export type UserRemoveSelectedAction = {
+	type: 'userRemoveSelected';
+};
+
+export type UsersStoredAction = {
+	type: 'usersStored';
+	payload: {
+		users: User[];
+	};
 };
 
 export type IncrementAction = {
@@ -25,12 +65,24 @@ export type DecrementAction = {
 	};
 };
 
-type Action = IncrementAction | DecrementAction;
+type Action =
+	| IncrementAction
+	| DecrementAction
+	| UserSelectedAction
+	| UserRemoveSelectedAction
+	| UsersStoredAction;
+
+const initialUsersState: UsersState = {
+	entities: {},
+	ids: [],
+	selectedUserId: undefined,
+};
 
 const inintialCounterState: CounterState = { counter: 0 };
 
 const initialState: State = {
 	counters: {},
+	users: initialUsersState,
 };
 
 // reducer - принимает предыдущее состояние и action далее возвращает новое состояние
@@ -65,6 +117,42 @@ const reducer = (state = initialState, action: Action): State => {
 				},
 			};
 		}
+		case 'usersStored': {
+			const { users } = action.payload;
+			return {
+				...state,
+				users: {
+					...state.users,
+					entities: users.reduce((acc, user) => {
+						acc[user.id] = user;
+						return acc;
+					}, {} as Record<UserId, User>),
+					ids: users.map(user => user.id),
+				},
+			};
+		}
+
+		case 'userSelected': {
+			const { userId } = action.payload;
+			return {
+				...state,
+				users: {
+					...state.users,
+					selectedUserId: userId,
+				},
+			};
+		}
+
+		case 'userRemoveSelected': {
+			return {
+				...state,
+				users: {
+					...state.users,
+					selectedUserId: undefined,
+				},
+			};
+		}
+
 		default:
 			return state;
 	}
@@ -73,6 +161,11 @@ const reducer = (state = initialState, action: Action): State => {
 export const store = configureStore({
 	reducer: reducer,
 });
+
+store.dispatch({
+	type: 'usersStored',
+	payload: { users },
+} satisfies UsersStoredAction);
 
 // selector - это чистая функция, которая принимает состояние и возвращает его какой то кусочек.
 // Важно, что это не должно создавать никаких объектов внутри себя итд
--- src/users-list.tsx
+++ src/users-list.tsx
@@ -1,102 +1,100 @@
-import { useState } from "react";
-
-type UserId = string;
-type User = {
-  id: UserId;
-  name: string;
-  description: string;
-};
-
-const users: User[] = Array.from({ length: 3000 }, (_, index) => ({
-  id: `user${index + 11}`,
-  name: `User ${index + 11}`,
-  description: `Description for User ${index + 11}`,
-}));
+import { useState, useMemo } from 'react';
+import {
+	UserId,
+	User,
+	UserRemoveSelectedAction,
+	UserSelectedAction,
+	useAppDispatch,
+	useAppSelector,
+} from './store';
 
 export function UsersList() {
-  const [selectedUser, setSelectedUser] = useState<User>();
+	const [sortType, setSortType] = useState<'asc' | 'desc'>('asc');
+	const ids = useAppSelector(state => state.users.ids);
+	const entities = useAppSelector((state) => state.users.entities);
+	const selectedUserId = useAppSelector(state => state.users.selectedUserId);
 
-  const [sortType, setSortType] = useState<"asc" | "desc">("asc");
+	const selectedUser = selectedUserId ? entities[selectedUserId] : undefined;
 
-  const handleUserClick = (user: User) => {
-    setSelectedUser(user);
-  };
+	const sortedUsers = useMemo(() => ids
+	.map(id => entities[id])
+	.sort((a, b) => {
+		if (sortType === "asc") {
+			return a.name.localeCompare(b.name);
+		} else {
+			return b.name.localeCompare(a.name);
+		}
+	}),
+	[ids, entities, sortType]
+);
 
-  const handleBackButtonClick = () => {
-    setSelectedUser(undefined);
-  };
+	
 
-  const sortedUsers = users.sort((a, b) => {
-    if (sortType === "asc") {
-      return a.name.localeCompare(b.name);
-    } else {
-      return b.name.localeCompare(a.name);
-    }
-  });
-
-  return (
-    <div className="flex flex-col items-center">
-      {!selectedUser ? (
-        <div className="flex flex-col items-center justify-between">
-          <div className="flex flex-row items-center">
-            <button
-              onClick={() => setSortType("asc")}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Asc
-            </button>
-            <button
-              onClick={() => setSortType("desc")}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
-            >
-              Desc
-            </button>
-          </div>
-          <ul className="list-none">
-            {sortedUsers.map((user) => (
-              <UserListItem
-                onClick={() => handleUserClick(user)}
-                user={user}
-                key={user.id}
-              />
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <SelectedUser
-          user={selectedUser}
-          onBackButtonClick={handleBackButtonClick}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center'>
+			{!selectedUser ? (
+				<div className='flex flex-col items-center justify-between'>
+					<div className='flex flex-row items-center'>
+						<button
+							onClick={() => setSortType('asc')}
+							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+						>
+							Asc
+						</button>
+						<button
+							onClick={() => setSortType('desc')}
+							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2'
+						>
+							Desc
+						</button>
+					</div>
+					<ul className='list-none'>
+						{sortedUsers.map(user => (
+							<UserListItem user={user} key={user.id} />
+						))}
+					</ul>
+				</div>
+			) : (
+				<SelectedUser user={selectedUser} />
+			)}
+		</div>
+	);
 }
 
-function UserListItem({ user, onClick }: { user: User; onClick: () => void }) {
-  return (
-    <li key={user.id} className="py-2" onClick={onClick}>
-      <span className="hover:underline cursor-pointer">{user.name}</span>
-    </li>
-  );
+// теперь click можно делать через dispatch внутри компонента, значит обращение к store не зависит от иерархии компонетов
+
+function UserListItem({ user }: { user: User }) {
+	const dispatch = useAppDispatch();
+	const handleUserClick = () => {
+		dispatch({
+			type: 'userSelected',
+			payload: { userId: user.id },
+		} satisfies UserSelectedAction);
+	};
+	return (
+		<li key={user.id} className='py-2' onClick={handleUserClick}>
+			<span className='hover:underline cursor-pointer'>{user.name}</span>
+		</li>
+	);
 }
 
-function SelectedUser({
-  user,
-  onBackButtonClick,
-}: {
-  user: User;
-  onBackButtonClick: () => void;
-}) {
-  return (
-    <div className="flex flex-col items-center">
-      <button
-        onClick={onBackButtonClick}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded md"
-      >
-        Back
-      </button>
-      <h2 className="text-3xl">{user.name}</h2>
-      <p className="text-xl">{user.description}</p>
-    </div>
-  );
+function SelectedUser({ user }: { user: User }) {
+	const dispatch = useAppDispatch();
+	const handleBackButtonClick = () => {
+		dispatch({
+			type: "userRemoveSelected"
+		} satisfies UserRemoveSelectedAction);
+	};
+	return (
+		<div className='flex flex-col items-center'>
+			<button
+				onClick={handleBackButtonClick}
+				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded md'
+			>
+				Back
+			</button>
+			<h2 className='text-3xl'>{user.name}</h2>
+			<p className='text-xl'>{user.description}</p>
+		</div>
+	);
 }
