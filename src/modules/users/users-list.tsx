9607154f76a51dda660ed9a4d4c48cbb2257f371
k1ego <ladonin05@mail.ru>
2024-07-03 18:53:18 +0300

    asyncThunk теперь определен внутри createSlice

--- src/modules/users/users-list.tsx
+++ src/modules/users/users-list.tsx
@@ -1,66 +1,66 @@
-import { memo, useState } from "react";
-import { useAppSelector } from "../../shared/redux";
-import { UserId, usersSlice } from "./users.slice";
-import { useNavigate } from "react-router-dom";
+import { memo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../shared/redux';
+import { UserId, usersSlice } from './users.slice';
 
 export function UsersList() {
-  const [sortType, setSortType] = useState<"asc" | "desc">("asc");
+	const [sortType, setSortType] = useState<'asc' | 'desc'>('asc');
 
-  const isPending = useAppSelector(
-    usersSlice.selectors.selectIsFetchUsersPending
-  );
+	const isPending = useAppSelector(
+		usersSlice.selectors.selectIsFetchUsersPending
+	);
 
-  const sortedUsers = useAppSelector((state) =>
-    usersSlice.selectors.selectSortedUsers(state, sortType)
-  );
+	const sortedUsers = useAppSelector(state =>
+		usersSlice.selectors.selectSortedUsers(state, sortType)
+	);
 
-  if (isPending) {
-    return <div>Loading...</div>;
-  }
+	if (isPending) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col items-center justify-between">
-        <div className="flex flex-row items-center">
-          <button
-            onClick={() => setSortType("asc")}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Asc
-          </button>
-          <button
-            onClick={() => setSortType("desc")}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
-          >
-            Desc
-          </button>
-        </div>
-        <ul className="list-none">
-          {sortedUsers.map((user) => (
-            <UserListItem userId={user.id} key={user.id} />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center'>
+			<div className='flex flex-col items-center justify-between'>
+				<div className='flex flex-row items-center'>
+					<button
+						onClick={() => setSortType('asc')}
+						className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					>
+						Asc
+					</button>
+					<button
+						onClick={() => setSortType('desc')}
+						className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2'
+					>
+						Desc
+					</button>
+				</div>
+				<ul className='list-none'>
+					{sortedUsers.map(user => (
+						<UserListItem userId={user.id} key={user.id} />
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 const UserListItem = memo(function UserListItem({
-  userId,
+	userId,
 }: {
-  userId: UserId;
+	userId: UserId;
 }) {
-  const navigate = useNavigate();
-  const user = useAppSelector((state) => state.users.entities[userId]);
-  const handleUserClick = () => {
-    navigate(userId, { relative: "path" });
-  };
-  if (!user) {
-    return null;
-  }
-  return (
-    <li key={user.id} className="py-2" onClick={handleUserClick}>
-      <span className="hover:underline cursor-pointer">{user.name}</span>
-    </li>
-  );
+	const navigate = useNavigate();
+	const user = useAppSelector(state => state.users.entities[userId]);
+	const handleUserClick = () => {
+		navigate(userId, { relative: 'path' });
+	};
+	if (!user) {
+		return null;
+	}
+	return (
+		<li key={user.id} className='py-2' onClick={handleUserClick}>
+			<span className='hover:underline cursor-pointer'>{user.name}</span>
+		</li>
+	);
 });
